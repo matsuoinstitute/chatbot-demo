8dc82d92bd6b09d5a44c3176fc46c152b4682daa
Toma.Kakizaki <t.kakizaki@neoai.jp>
2024-02-21 22:38:14 +0000

    リファクタ

--- src/pdf_app.py
+++ src/pdf_app.py
@@ -2,12 +2,10 @@ import os
 
 import chainlit as cl
 from dotenv import load_dotenv
-from langchain.chains import ConversationalRetrievalChain, LLMChain
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
-from langchain.prompts import PromptTemplate
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
 
@@ -29,18 +27,6 @@ ALLOWED_EXTENSIONS = [
     ".xlsx",
 ]
 
-prompt = PromptTemplate(
-    template="""
-    文章を前提にして質問に答えてください。
-
-    文章 :
-    {document}
-
-    質問 : {question}
-    """,
-    input_variables=["document", "question"],
-)
-
 
 @cl.on_chat_start
 async def on_chat_start():
@@ -49,15 +35,10 @@ async def on_chat_start():
 
     # awaitメソッドのために、whileを利用する。アップロードされるまで続く。
     while files is None:
-        # chainlitの機能に、ファイルをアップロードさせるメソッドがある。
         files = await cl.AskFileMessage(
-            # ファイルの最大サイズ
             max_size_mb=20,
-            # ファイルをアップロードさせる画面のメッセージ
             content="ファイルを選択してください（.pdf、.docx、.xlsxに対応しています）",
-            # PDFファイルを指定する
             accept=ALLOWED_MIME_TYPES,
-            # タイムアウトなし
             raise_on_timeout=False,
         ).send()
 
@@ -85,10 +66,9 @@ async def on_chat_start():
         # PDFから内容を分割されたドキュメントを保存する。
         database.add_documents(splitted_documents)
 
-        # 今回は、簡易化のためセッションに保存する。
         cl.user_session.set("data", database)
         await cl.Message(content="アップロードが完了しました！").send()
-    
+
     llm = ChatOpenAI(model="gpt-4-0125-preview", temperature=0)
     message_history = ChatMessageHistory()
     memory = ConversationBufferMemory(
@@ -98,6 +78,7 @@ async def on_chat_start():
         return_messages=True,
     )
 
+    # チャットモデルを初期化する。
     if database is not None:
         retriever = database.as_retriever()
         qa = ConversationalRetrievalChain.from_llm(
@@ -116,6 +97,7 @@ async def on_message(input_message: cl.Message):
     """メッセージが送られるたびに呼び出される."""
     qa = cl.user_session.get("qa")
 
+    # ユーザーの入力をQAモデルに渡して、回答を取得する。
     res = await qa.acall(
         input_message.content,
         callbacks=[cl.AsyncLangchainCallbackHandler()]
@@ -124,6 +106,7 @@ async def on_message(input_message: cl.Message):
     source_documents = res["source_documents"]
     text_elements = []
 
+    # 参照元のドキュメントを表示する。
     if source_documents:
         for source_idx, source_doc in enumerate(source_documents):
             source_name = f"source_{source_idx}"
