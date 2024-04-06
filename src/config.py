061c0a6ecbfc2ba7785f0c4362b1afd53b4d48e1
sora <sora0306122426@gmail.com>
2024-04-06 20:02:44 +0900

    Add config.py, update requirements, and demo.py

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+# .envファイルへの相対パス
+env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path=env_path)
+
+# 環境変数を取得
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
--- src/demo.py
+++ src/demo.py
@@ -1,24 +1,38 @@
+#https://qiita.com/skkzsh/items/17bff703dba27240ec90のほぼコピー
+
+#環境変数の読み込み
+import config
+
+openai_client_key = config.OPENAI_API_KEY
+
+#必要なライブラリの読み込み
 import chainlit as cl
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+from langchain_openai import ChatOpenAI
 
 
-@cl.step
-def tool():
-    return "Response from the tool!"
+#chainlitとlangchainを利用してチャットボットのUIと機能を作る。
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatOpenAI(streaming=True)
+    prompt = ChatPromptTemplate.from_messages([("human", "{question}"),
+        ])
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
 
-@cl.on_message  # this function will be called every time a user inputs a message in the UI
-async def main(message: cl.Message):
-    """
-    This function is called every time a user inputs a message in the UI.
-    It sends back an intermediate response from the tool, followed by the final answer.
-    Args:
-        message: The user's message.
-    Returns:
-        None.
-    """
+    msg = cl.Message(content="")
 
-    # Call the tool
-    tool()
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
 
-    # Send the final answer.
-    await cl.Message(content="This is the final answer").send()
+    await msg.send()
